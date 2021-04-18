4ee7cf1666e79e5bdbf66a0b6b0de608444bd813
kobayashi kantaro <buhi58ms@gmail.com>
2021-04-18 20:51:49 +0900

    一旦

--- morningApp/views.py
+++ morningApp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'morningApp/index.html', {})
+    question = Question.objects.all()
+    header = ['質問内容', '選択肢1', '選択肢2', '選択肢3', '選択肢4']
+    info = {
+        'val': question,
+        'header': header,
+    }
+    return render(request, 'morningApp/index.html', info)
 
 def test(request):
     return render(request, 'morningApp/test.html', {})
